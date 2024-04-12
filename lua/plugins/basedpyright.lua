e3e35c89cb3bd50a3c9e4bed90af69b8b9427cc4
Thomas Phipps <thomas.phipps@broadcom.com>
2024-04-12 22:04:32 +0000

    fix pyrigh config

--- lua/plugins/basedpyright.lua
+++ lua/plugins/basedpyright.lua
@@ -1,4 +1,7 @@
 local lspconfig = require("lspconfig")
+local configs = require "nvchad.configs.lspconfig"
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
 
 return {
   lspconfig.basedpyright.setup{
