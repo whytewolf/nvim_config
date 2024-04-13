9ca2e3c85667ffd90126959b27abe2d80c0c00f5
Thomas Phipps <thomas.phipps@broadcom.com>
2024-04-13 03:39:59 +0000

    more git setup

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,7 +7,35 @@ local git_plugins = {
         require('telescope').load_extension('gh')
       end,
     },
-  }
+  },
+  {
+    "tpope/vim-fugitive",
+    event = "VeryLazy",
+    dependencies = {
+      "tpope/vim-rhubarb",
+      "tpope/vim-obsession",
+      "tpope/vim-unimpaired",
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function ()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "󰍵" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "│" },
+        },
+        signcolumn = true,
+      })
+    end,
+  },
+  {
+    'hougesen/blame-me.nvim',
+  },
 }
 
 return git_plugins
