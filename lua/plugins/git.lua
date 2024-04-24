7d5bd2acbd4880fb4a414eaf24f6d62637b29153
Thomas Phipps <thomas.phipps@broadcom.com>
2024-04-24 21:49:06 +0000

    add telescope to lazygit
    added telescope t lazygit as a way of being awesome

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -42,8 +42,12 @@ local git_plugins = {
     	},
         -- optional for floating window border decoration
         dependencies = {
+            "nvim-telescope/telescope.nvim",
             "nvim-lua/plenary.nvim",
         },
+        config = function()
+          require("telescope").load_extension("lazygit")
+        end,
         -- setting the keybinding for LazyGit with 'keys' is recommended in
         -- order to load the plugin when the command is run for the first time
         keys = {
