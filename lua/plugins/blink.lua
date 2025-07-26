419beaba9cd58d36514f7ea6243718331b507e59
Thomas Phipps <oldtiredbastard@gmail.com>
2025-07-25 19:06:16 -0700

    updates, blink config.
    added copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,17 +1,36 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono',
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
     },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
-  opts_extend = { 'sources.default' },
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot' },
+    version = '1.*',
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+      completion = { documentation = { auto_show = false } },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    },
+    opts_extend = { 'sources.default' },
+  },
 }
