2f0709b1f24e205e72362e59df0cb1895b6ccb3e
Thomas Phipps <oldtiredbastard@gmail.com>
2025-08-06 19:29:02 -0700

    add nerd glyph lookup
    added nerd glyph lookup plugin. update some plugins, rework the
    dashboard a bit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,16 +7,44 @@ return {
     bigfile = { enabled = true },
     dashboard = {
       enabled = true,
+      preset = {
+        keys = {
+          { icon = ' ', key = 'M', desc = 'Mason', action = ':Mason' },
+          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+          { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        },
+      },
       sections = {
         {
           section = 'terminal',
-          cmd = 'chafa ~/.config/nvim/watch.small.jpg --format symbols --symbols vhalf --size 60x17 --stretch',
+          cmd = 'chafa ~/.config/nvim/watch.small.jpg --probe off --format symbols --symbols vhalf --size 60x17 --stretch',
           height = 17,
           padding = 1,
         },
         {
           pane = 2,
           { section = 'keys', gap = 1, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          {
+            icon = ' ',
+            title = 'Git Status',
+            section = 'terminal',
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = 'git status --short --branch --renames',
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
           { section = 'startup' },
         },
       },
