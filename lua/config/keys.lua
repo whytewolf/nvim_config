0314722db9a90f9763d6ef2c2d3a4ea11c5b01ae
Thomas Phipps <oldtiredbastard@gmail.com>
2025-08-26 22:09:36 -0700

    minor changes
    some very minor changes.
    
    * move keymaps from options to keys.
    * turned off perl_provider, who uses perl anymore
    * some minor updates including setting versions for conform and
    codecompanion

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -3,6 +3,14 @@ local dapui = require 'dapui'
 local dwidgts = require 'dap.ui.widgets'
 local keymap = vim.keymap
 
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 -- dap keymaps
 keymap.set('n', '<leader>do', function()
   dapui.open()
--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -2,6 +2,7 @@ return {
   {
     'olimorris/codecompanion.nvim',
     opts = {},
+    version = '^17',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
