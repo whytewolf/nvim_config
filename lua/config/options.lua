d4890269e5f63f607821f01dbeb98bf4efc4e256
Thomas Phipps <oldtiredbastard@gmail.com>
2025-09-13 19:00:30 -0700

    hopfully this works
    setting up nvim to use powershell in windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -54,3 +54,14 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 vim.g.loaded_perl_provider = 0
+
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  vim.o.shell = 'pwsh'
+  vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+  vim.o.shellquote = ''
+  vim.o.shellxquote = ''
+  vim.o.shellpipe = '| Out-File -Encoding UTF8'
+  vim.o.shellredir = '> Out-File -Encoding UTF8'
+  vim.o.shellxescape = '"'
+else
+end
