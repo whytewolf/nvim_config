56526feff818712fcab3fc5eff291e3f4414bff4
Thomas Phipps <oldtiredbastard@gmail.com>
2026-08-09 15:20:25 -0700

    updated

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,6 +38,7 @@ vim.pack.add({
   'https://github.com/folke/noice.nvim',
   'https://github.com/MunifTanjim/nui.nvim',
   'https://github.com/rcarriga/nvim-notify',
+  'https://github.com/mr-u0b0dy/crazy-coverage.nvim',
 })
 
 require("plugins.aerial")
@@ -55,3 +56,4 @@ require("plugins.pixel")
 require("plugins.smear")
 require("plugins.lualine")
 require("plugins.noice")
+require("plugins.cov")
